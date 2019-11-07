44a3cde8eb6290419240d1bf6c086367bb6a4fe8
Nathra21 <etienneboisseau804@gmail.com>
2019-11-07 17:24:41 +0100

    NGramModel now parses tuples of strings instead of space-separated tokens

--- code/ngram/ngram.py
+++ code/ngram/ngram.py
@@ -8,11 +8,11 @@ class NGramModel:
         self.freq_n_grams = {}
     
     @staticmethod
-    def counting_kgrams(text_tokens, k):
+    def count_kgrams(text_tokens, k):
         freq_kgrams = {}
-        for i in range(0,len(text_tokens)-k+1):
-            kgram = ' '.join(text_tokens[i:i+k])
-            freq_kgrams[kgram] = freq_kgrams.get(kgram,0)+1
+        for i in range(0, len(text_tokens)-k+1):
+            kgram = tuple(text_tokens[i:i+k])
+            freq_kgrams[kgram] = freq_kgrams.get(kgram, 0) + 1
         return freq_kgrams
         
     def fit(self, text_tokens, verbose=True):
@@ -26,40 +26,54 @@ class NGramModel:
         if verbose:
             print("Fitting {}-gram model on vocabulary of size {}.".format(self.n, len(self.vocabulary)))
         
-        self.freq_n_minus_one_grams = self.counting_kgrams(text_tokens, self.n-1)
-        self.freq_n_grams = self.counting_kgrams(text_tokens, self.n)
+        self.freq_n_minus_one_grams = self.count_kgrams(text_tokens, self.n-1)
+        self.freq_n_grams = self.count_kgrams(text_tokens, self.n)
     
     def get_word_probability(self, word, previous_words):
         """Get the probability of a word given the n-1 previous words.
         
         Args:
             word (str): Word to predict probability of.
-            previous_words (string): Words which precede `word` (separated by space). Must be of length self.n - 1.
+            previous_words (tuple of strings): Words which precede `word`. Must be of length self.n - 1.
         """
-        nb_previous_words = len(previous_words.split(" "))
-        assert nb_previous_words == self.n - 1, "Error in probability calculation: invalid number of previous words: {}".format(nb_previous_words)
+        assert len(previous_words) == self.n - 1, "Error in probability calculation: invalid number of previous words: {}".format(len(previous_words))
         
         if self.freq_n_minus_one_grams.get(previous_words, 0) == 0:
             proba = 0
         else:
             proba = (
-                self.freq_n_grams.get(previous_words + " " + word, 0)
+                self.freq_n_grams.get(previous_words + (word,), 0)
                 / self.freq_n_minus_one_grams.get(previous_words, 0)
             )
         
         return proba
     
     def generate_greedy(self, nb_words_to_gen, previous_words):
+        """Generate a sequence of words starting from given starting words.
+        
+        Args:
+            nb_words_to_gen (int): Number of words to generate past the starting sequence.
+            previous_words (str or iterable): List of tokens to start from. If string, must be
+                sequence of tokens separated by spaces.
+        
+        Returns:
+            tuple of strings: Generated tokens.
+        """
+        # Sanitize input
+        if isinstance(previous_words, str):
+            previous_words = previous_words.split(" ")
+        previous_words = tuple(previous_words)
+        
         for i in range(nb_words_to_gen):
             cond_probas=[]
             for word in self.vocabulary:
                 cond_probas.append(
                     self.get_word_probability(
                         word,
-                        " ".join(previous_words.split(' ')[-(self.n-1):])
+                        previous_words[-(self.n-1):]
                     )
                 )
-            #Selectionne le mot qui maximise la probabilité conditionnelle (greedy search)
-            previous_words += " " + self.vocabulary[np.argmax(cond_probas)]
+            # Sélectionne le mot qui maximise la probabilité conditionnelle (greedy search)
+            previous_words += (self.vocabulary[np.argmax(cond_probas)],)
         return previous_words
     
