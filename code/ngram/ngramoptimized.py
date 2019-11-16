c190be7520b6852513220cbac1a3e6d0658ef4bc
CeluiQuiSait <eric.lavergne@telecom-sudparis.eu>
2019-11-17 00:09:42 +0100

    implementation de l'optimisation boisseaunienne pour greedy et sample methods (et la prediction est en effet BCP plus rapide), code à refacto

---
+++ code/ngram/ngramoptimized.py
@@ -0,0 +1,174 @@
+import numpy as np
+from random import choice
+
+
+class NGramModel:
+    
+    def __init__(self, n):
+        self.n = n
+        self.vocabulary = []
+        self.conditional_probabilities = {}
+    
+    
+    def compute_conditional_probabilities(self, text_tokens):
+        """Compute conditional probabilities on pre-tokenized text counting ngrams.
+        
+        Args:
+            text_tokens (list of strings): Tokenized text.
+        """
+        counts = {}
+        
+        for i in range(0, len(text_tokens)-self.n+1):
+            n_minus_1_gram = tuple(text_tokens[i:i+self.n-1])
+            next_world = text_tokens[i+self.n-1]
+            if n_minus_1_gram not in counts:
+                counts[n_minus_1_gram] = {}
+            counts[n_minus_1_gram][next_world] = counts[n_minus_1_gram].get(next_world, 0) + 1
+            
+        for n_minus_1_gram, possible_next_worlds in counts.items():
+            counts_sum = sum(list(possible_next_worlds.values()))
+            for next_world in possible_next_worlds:
+                counts[n_minus_1_gram][next_world] /= counts_sum
+                
+        self.conditional_probabilities = counts
+        
+        
+    def fit(self, text_tokens, verbose=True):
+        """Fit the n-gram model on pre-tokenized text based on the n attribute of the model.
+        
+        Args:
+            text_tokens (list of strings): Tokenized text.
+            verbose (bool): Whether to print extra info.
+        """
+        self.vocabulary = list(set(text_tokens))
+        if verbose:
+            print("Fitting {}-gram model on vocabulary of size {}.".format(self.n, len(self.vocabulary)))
+        
+        self.compute_conditional_probabilities(text_tokens)
+    
+    
+    def get_word_probability(self, word, previous_words):
+        """Get the probability of a word given the n-1 previous words.
+        
+        Args:
+            word (str): Word to predict probability of.
+            previous_words (tuple of strings): Words which precede `word`. Must be of length self.n - 1.
+        """
+        assert len(previous_words) == self.n - 1, "Error in probability calculation: invalid number of previous words: {}".format(len(previous_words))
+        
+        if previous_words not in self.conditional_probabilities:
+            proba = 0
+        else:
+            proba = self.conditional_probabilities[previous_words].get(word,0)
+        
+        return proba
+    
+    
+    def predict_probas(self, previous_words):
+        """Get all conditional word probabilities given previous n-1 words.
+        
+        Args:
+            previous_words (tuple of strings): Previous sequence of tokens. Must be of length at least n-1.
+        
+        Returns:
+            list: vector of probabilities of same size as vocabulary.
+        """
+        assert len(previous_words) >= self.n - 1, "Error in probability calculation: invalid number of previous words: {}".format(len(previous_words))
+
+        cond_probas=[]
+        for word in self.vocabulary:
+            cond_probas.append(
+                self.get_word_probability(
+                    word,
+                    previous_words[-(self.n-1):]
+                )
+            )
+        return cond_probas
+
+    
+    def generate_greedy(self, nb_words_to_gen, previous_words):
+        """Generate a sequence of words starting from given starting words using greedy prediction.
+        
+        Args:
+            nb_words_to_gen (int): Number of words to generate past the starting sequence.
+            previous_words (str or iterable): List of tokens to start from. If string, must be
+                sequence of tokens separated by spaces.
+        
+        Returns:
+            tuple of strings: Generated tokens.
+        """
+        # Sanitize input
+        if isinstance(previous_words, str):
+            previous_words = previous_words.split(" ")
+        previous_words = tuple(previous_words)
+        
+        for i in range(nb_words_to_gen):
+            cond_probas = self.predict_probas(previous_words)
+            # Sélectionne le mot qui maximise la probabilité conditionnelle (greedy search)
+            next_word = self.vocabulary[np.argmax(cond_probas)]
+            #Un peu sale mais évite deux parcours du dico ; idee de code plus propre ?
+            possible_next_words_probas = self.conditional_probabilities.get(previous_words[-(self.n-1):], {choice(self.vocabulary): 1})
+            next_word = max(possible_next_words_probas, key=possible_next_words_probas.get)
+            previous_words += (next_word,)
+        return previous_words
+    
+    
+    def generate_sampled(self, nb_words_to_gen, previous_words, power=1):
+        """Generate a sequence of words starting from given starting words using the sampling method.
+        
+        Args:
+            nb_words_to_gen (int): Number of words to generate past the starting sequence.
+            previous_words (str or iterable): List of tokens to start from. If string, must be
+                sequence of tokens separated by spaces.
+            power (float): Power to raise probabilities at before sampling.
+                A higher power means a less risky sampling.
+                An infinite power would be equivalent to greedy sampling.
+        
+        Returns:
+            tuple of strings: Generated tokens.
+        """
+        # Sanitize input
+        if isinstance(previous_words, str):
+            previous_words = previous_words.split(" ")
+        previous_words = tuple(previous_words)
+        
+        for i in range(nb_words_to_gen):
+            cond_probas = self.predict_probas(previous_words)
+            cond_probas = np.array(cond_probas)**power
+            cond_probas = cond_probas / cond_probas.sum()
+            # Sample a word from conditional distribution
+            next_word = np.random.choice(self.vocabulary, p=cond_probas)
+            previous_words += (next_word,)
+        return previous_words
+    
+    
+    def generate_beam(self, nb_words_to_gen, previous_words, k=1):
+        """Generate a sequence of words starting from given starting words using beam prediction.
+        
+        Args:
+            nb_words_to_gen (int): Number of words to generate past the starting sequence.
+            previous_words (str or iterable): List of tokens to start from. If string, must be
+                sequence of tokens separated by spaces.
+            k (int): parameter of Beam search method, i.e. number of best sequences kept at each step
+        
+        Returns:
+            tuple of strings : Generated tokens.
+        """
+        # Sanitize input
+        if isinstance(previous_words, str):
+            previous_words = previous_words.split(" ")
+        previous_words = tuple(previous_words)
+        
+        k_best_sequences = [[previous_words,1]]
+        for i in range(nb_words_to_gen):
+            candidates = []
+            for best_sequence in k_best_sequences:
+                previous_words, previous_probability = best_sequence
+                for word in self.vocabulary:
+                    sequence_probability = previous_probability * self.get_word_probability(word, previous_words[-(self.n-1):])
+                    sequence_words = previous_words + (word,)
+                    candidates.append([sequence_words, sequence_probability])
+            ordered = sorted(candidates, key=lambda tup:tup[1])
+            k_best_sequences = ordered[-k:]
+        return k_best_sequences[-1][0]
+    
