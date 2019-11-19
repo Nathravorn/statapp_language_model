ba4b8f3e5bd6bd111c92459053b824e40c18fb4e
Nathra21 <etienneboisseau804@gmail.com>
2019-11-19 18:43:32 +0100

    Rename ngramoptimized to ngram

--- code/ngram/ngram.py
+++ code/ngram/ngram.py
@@ -1,19 +1,14 @@
 import numpy as np
+from random import choice
+
 
 class NGramModel:
+    
     def __init__(self, n):
         self.n = n
         self.vocabulary = []
-        self.freq_n_minus_one_grams = {}
-        self.freq_n_grams = {}
-    
-    @staticmethod
-    def count_kgrams(text_tokens, k):
-        freq_kgrams = {}
-        for i in range(0, len(text_tokens)-k+1):
-            kgram = tuple(text_tokens[i:i+k])
-            freq_kgrams[kgram] = freq_kgrams.get(kgram, 0) + 1
-        return freq_kgrams
+        self.conditional_probabilities = {}
+        
         
     def fit(self, text_tokens, verbose=True):
         """Fit the n-gram model on pre-tokenized text based on the n attribute of the model.
@@ -26,8 +21,28 @@ class NGramModel:
         if verbose:
             print("Fitting {}-gram model on vocabulary of size {}.".format(self.n, len(self.vocabulary)))
         
-        self.freq_n_minus_one_grams = self.count_kgrams(text_tokens, self.n-1)
-        self.freq_n_grams = self.count_kgrams(text_tokens, self.n)
+        counts = {}
+        
+        # Iterating through the tokens
+        for i in range(0, len(text_tokens) - self.n + 1):
+            n_minus_1_gram = tuple(text_tokens[i:i+self.n-1])
+            next_word = text_tokens[i+self.n-1]
+            
+            if n_minus_1_gram not in counts:
+                counts[n_minus_1_gram] = {}
+            
+            if next_word not in counts[n_minus_1_gram]:
+                counts[n_minus_1_gram][next_word] = 0
+                
+            counts[n_minus_1_gram][next_word] += 1
+            
+        for n_minus_1_gram, possible_next_words in counts.items():
+            counts_sum = sum(list(possible_next_words.values()))
+            for next_word in possible_next_words:
+                counts[n_minus_1_gram][next_word] /= counts_sum
+                
+        self.conditional_probabilities = counts
+    
     
     def get_word_probability(self, word, previous_words):
         """Get the probability of a word given the n-1 previous words.
@@ -38,36 +53,27 @@ class NGramModel:
         """
         assert len(previous_words) == self.n - 1, "Error in probability calculation: invalid number of previous words: {}".format(len(previous_words))
         
-        if self.freq_n_minus_one_grams.get(previous_words, 0) == 0:
+        if previous_words not in self.conditional_probabilities:
             proba = 0
         else:
-            proba = (
-                self.freq_n_grams.get(previous_words + (word,), 0)
-                / self.freq_n_minus_one_grams.get(previous_words, 0)
-            )
+            proba = self.conditional_probabilities[previous_words].get(word,0)
         
         return proba
     
-    def predict_probas(self, previous_words):
-        """Get all conditional word probabilities given previous n-1 words.
+    
+    def predict_possible_next_words_probas(self, previous_words):
+        """Get all conditional word probabilities greater than zero given previous n-1 words.
         
         Args:
             previous_words (tuple of strings): Previous sequence of tokens. Must be of length at least n-1.
         
         Returns:
-            list: vector of probabilities of same size as vocabulary.
+            dictionnary: conditional probabilities (float) with keys .
         """
         assert len(previous_words) >= self.n - 1, "Error in probability calculation: invalid number of previous words: {}".format(len(previous_words))
-
-        cond_probas=[]
-        for word in self.vocabulary:
-            cond_probas.append(
-                self.get_word_probability(
-                    word,
-                    previous_words[-(self.n-1):]
-                )
-            )
-        return cond_probas
+        
+        #Un peu sale mais évite deux parcours du dico ; idee de code plus propre ?
+        return self.conditional_probabilities.get(previous_words[-(self.n-1):], {choice(self.vocabulary): 1})
 
     
     def generate_greedy(self, nb_words_to_gen, previous_words):
@@ -87,11 +93,12 @@ class NGramModel:
         previous_words = tuple(previous_words)
         
         for i in range(nb_words_to_gen):
-            cond_probas = self.predict_probas(previous_words)
             # Sélectionne le mot qui maximise la probabilité conditionnelle (greedy search)
-            next_word = self.vocabulary[np.argmax(cond_probas)]
+            possible_next_words_probas = self.predict_possible_next_words_probas(previous_words)
+            next_word = max(possible_next_words_probas, key=possible_next_words_probas.get)
             previous_words += (next_word,)
         return previous_words
+    
     
     def generate_sampled(self, nb_words_to_gen, previous_words, power=1):
         """Generate a sequence of words starting from given starting words using the sampling method.
@@ -113,15 +120,18 @@ class NGramModel:
         previous_words = tuple(previous_words)
         
         for i in range(nb_words_to_gen):
-            cond_probas = self.predict_probas(previous_words)
+            possible_next_words_probas = self.predict_possible_next_words_probas(previous_words)
+            cond_probas = list(possible_next_words_probas.values())
             cond_probas = np.array(cond_probas)**power
             cond_probas = cond_probas / cond_probas.sum()
+            possible_next_words = np.array(list(possible_next_words_probas.keys()))
             # Sample a word from conditional distribution
-            next_word = np.random.choice(self.vocabulary, p=cond_probas)
+            next_word = np.random.choice(possible_next_words, p=cond_probas)
             previous_words += (next_word,)
         return previous_words
     
-    def generate_beam(self, nb_words_to_gen, previous_words, k=1):
+    
+    def generate_beam(self, nb_words_to_gen, previous_words, k=3):
         """Generate a sequence of words starting from given starting words using beam prediction.
         
         Args:
@@ -143,8 +153,9 @@ class NGramModel:
             candidates = []
             for best_sequence in k_best_sequences:
                 previous_words, previous_probability = best_sequence
-                for word in self.vocabulary:
-                    sequence_probability = previous_probability * self.get_word_probability(word, previous_words[-(self.n-1):])
+                possible_next_words_probas = self.predict_possible_next_words_probas(previous_words)
+                for word, word_cond_probability in possible_next_words_probas.items():
+                    sequence_probability = previous_probability * word_cond_probability
                     sequence_words = previous_words + (word,)
                     candidates.append([sequence_words, sequence_probability])
             ordered = sorted(candidates, key=lambda tup:tup[1])
