adf5f9ef315e57e9c87169d31344ed77a21da6a5
Nathra21 <etienneboisseau804@gmail.com>
2019-12-14 21:33:32 +0100

    Tweak regr_lin_model to use optimizer

--- code/transformer/tensorflow/regr_lin_model.py
+++ code/transformer/tensorflow/regr_lin_model.py
@@ -10,8 +10,8 @@ class LinearModel:
         # otherwise, there is a type error
         # during training as __call__
         # would return a float32
-        self.a = tf.Variable(-2.0, dtype=tf.float64)
         self.b = tf.Variable(1.0, dtype=tf.float64)
+        self.a = tf.Variable(-2.0, dtype=tf.float64)
     
     def __call__(self, x):
         return self.a * x + self.b
@@ -24,7 +24,7 @@ a, b, N = 0.5, 3, 200
 X = np.linspace(0,3,N)
 Y = a * X + b + np.random.normal(0,1,N)
 
-#optimizer = tf.keras.optimizers.Adam(learning_rate=0.1)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.1)
 loss_fn = tf.keras.losses.MeanSquaredError()
 
 @tf.function
@@ -32,9 +32,9 @@ def train(model, x, y, loss_fn, l=0.1):
     with tf.GradientTape() as tape:
         loss = loss_fn(y, model(x))
     gradients = tape.gradient(loss, [model.a, model.b])
-    model.a.assign_sub(l * gradients[0])
-    model.b.assign_sub(l * gradients[1])
-    #optimizer.apply_gradients(zip(gradients, [model.a, model.b]))
+    # model.a.assign_sub(l * gradients[0])
+    # model.b.assign_sub(l * gradients[1])
+    optimizer.apply_gradients(zip(gradients, [model.a, model.b]))
 
 if __name__ == "__main__":
     linear_model = LinearModel()
