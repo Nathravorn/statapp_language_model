83c6fc8abfaa5169540c0763a38d703cacad5b14
Nathra21 <etienneboisseau804@gmail.com>
2019-12-16 15:28:00 +0100

    Add load_data function

--- statapp/transformer/common.py
+++ statapp/transformer/common.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow_datasets as tfds
 
 def get_positional_encodings(seq_length, d_model):
     """Compute transformer positional encodings.
@@ -25,33 +26,28 @@ def get_positional_encodings(seq_length, d_model):
     return encodings
 
 
-def load_data(path, sample=1, split_on=None):
+def load_data(path, sample=1, split_on=" "):
     """Load a text dataset, optionally splitting based on some splitting string.
     
     Args:
         path (str): Path to a text file.
         sample (float): (approximate) Proportion of the dataset to load. 1 loads everything.
-        If split_on is not None, samples the dataset post-split instead of pre-split.
-        If split_on is None, samples the dataset by cutting at the closest space character.
             Default: 1.
         split_on (str): String to split dataset on, e.g. "\n".
-            If not None, will shuffle before splitting.
-            Default: None.
+            When sampling, the dataset will be cut at the previous split_on substring.
+            If None, samples the dataset by cutting at the previous character.
+            Default: " ".
     
     Returns:
-        list of strings: Split dataset. If split_on is None, list has one element.
+        string: Dataset.
     """
     with open(path, "r") as file:
         text = file.read()
     
-    if split_on is None:
-        text = text.split(" ")
-        text = " ".join(text[:int(len(text)*sample)])
-        return text
+    text = text[:int(len(text)*sample)]
+    if split_on is not None:
+        text = text[:-text[::-1].find(split_on)-1]
     
-    else:
-        text = text.split(split_on)
-        if sample != 1:
-            text = np.random.choice(text, size=int(len(text)*sample))
-        
-        return text
+    return text
+    
+    
