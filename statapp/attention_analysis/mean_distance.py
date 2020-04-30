972ce55a3b22dd87dbc59c2e9f4e983d6fe37f07
Nathra21 <etienneboisseau804@gmail.com>
2020-04-30 19:08:11 +0200

    Slightly re-doc mean_distance

--- statapp/attention_analysis/mean_distance.py
+++ statapp/attention_analysis/mean_distance.py
@@ -2,19 +2,22 @@ import numpy as np
 import pandas as pd
 
 def compute_mean_distance(array):
-    """Calculate mean distance
+    """Calculate mean attention distance in given attention array.
     
     Args:
-        array (numpy array): Input array, with two last axis of same dimensions
+        array (numpy array): Array of attentions, with last two axes of same dimension (seq_length).
         
     Returns:
-        np.array: Array of mean distances, with two less dimensions (the two last axis) than the input array.
+        np.array: Array of mean distances, with two fewer dimensions (the last two axes) than the input array.
     """
     seq_length = array.shape[-1]
     assert seq_length == array.shape[-2], "The two last dimensions of the array are not equal"
     
-    distances = np.abs(np.arange(0,seq_length).repeat(seq_length).reshape(seq_length,-1)+np.arange(0,-seq_length,-1))
+    distances = np.abs(
+        np.arange(0,seq_length)
+        .repeat(seq_length)
+        .reshape(seq_length,-1)
+        + np.arange(0,-seq_length,-1)
+    )
     
     return (array*distances).mean(axis = (-1,-2))
-    
-    
